ac04f067730b79e8c57e8be4f8dd1bd1c68860a1
Qiwen Wang <qwang70@illinois.edu>
2019-11-12 00:23:06 -0800

    before I fell asleep

--- model/discriminator.py
+++ model/discriminator.py
@@ -7,7 +7,7 @@ class FCDiscriminator(nn.Module):
 	def __init__(self, num_classes, ndf = 64):
 		super(FCDiscriminator, self).__init__()
 
-		self.conv1 = nn.Conv2d(num_classes, ndf, kernel_size=4, stride=2, padding=1)
+		self.conv1 = nn.Conv2d(num_classes * 2, ndf, kernel_size=4, stride=2, padding=1)
 		self.conv2 = nn.Conv2d(ndf, ndf*2, kernel_size=4, stride=2, padding=1)
 		self.conv3 = nn.Conv2d(ndf*2, ndf*4, kernel_size=4, stride=2, padding=1)
 		self.conv4 = nn.Conv2d(ndf*4, ndf*8, kernel_size=4, stride=2, padding=1)
