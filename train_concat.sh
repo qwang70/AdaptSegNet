9276a2f6634f27406603f9ac8bdebe1f05c6bcb7
zhsun0357 <38968396+zhsun0357@users.noreply.github.com>
2019-11-14 11:26:59 -0800

    use concatenation rather than element wise mul

---
+++ train_concat.sh
@@ -0,0 +1,4 @@
+python3 train_gta2cityscapes_multi_0_4_cdan_concat.py --lambda-adv 0.001
+python3 train_gta2cityscapes_multi_0_4_cdan_concat.py --lambda-adv 0.01
+python3 train_gta2cityscapes_multi_0_4_cdan_concat.py --lambda-adv 0.1
+python3 train_gta2cityscapes_multi_0_4_cdan_concat.py --lambda-adv 0.005
